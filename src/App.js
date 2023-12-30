f578958a43f4926b457c6b4cc8bfdadaf87fe023
devesh <devesh7664@gmail.com>
2023-12-31 01:16:00 +0530

    chatbot started

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import ChatBot from './components/ChatBot/ChatBot';
+import './bootstrap/bootstrap.css';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <ChatBot />
+
+    </div>
+  );
+}
+
+export default App;
